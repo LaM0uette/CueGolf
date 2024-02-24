166a30df62c8c74c6c412eb7ecb0ca76d04123af
LaM0uette <LaM0uette.dev@gmail.com>
2024-02-24 14:59:22 +0100

    clean inputsReader

--- Assets/Game/Modules/Player/Inputs/InputsReader.cs
+++ Assets/Game/Modules/Player/Inputs/InputsReader.cs
@@ -15,23 +15,10 @@ namespace Game.Modules.Player.Inputs
 
         #endregion
 
-        private void OnEnable()
-        {
-            _playerSoap.PutterPressEvent.OnRaised += () => Debug.Log("Putter Pressed");
-            _playerSoap.PutterReleaseEvent.OnRaised += () => Debug.Log("Putter Released");
-        }
-
         #region Events
 
-        public void OnPutterPress()
-        {
-            _playerSoap.PutterPressEvent.Raise();
-        }
-        
-        public void OnPutterRelease()
-        {
-            _playerSoap.PutterReleaseEvent.Raise();
-        }
+        public void OnPutterPress() => _playerSoap.PutterPressEvent.Raise();
+        public void OnPutterRelease() => _playerSoap.PutterReleaseEvent.Raise();
 
         #endregion
     }
