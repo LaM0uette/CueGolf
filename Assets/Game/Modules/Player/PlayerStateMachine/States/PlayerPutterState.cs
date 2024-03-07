ec970c776c6001f798a6b23cc7138b4687c1b377
LaM0uette <LaM0uette.dev@gmail.com>
2024-03-07 18:07:49 +0100

    test

--- Assets/Game/Modules/Player/PlayerStateMachine/States/PlayerPutterState.cs
+++ Assets/Game/Modules/Player/PlayerStateMachine/States/PlayerPutterState.cs
@@ -66,10 +66,18 @@ namespace Game.Modules.Player.PlayerStateMachine.States
             direction.y = 0;
             
             // la rotation de la balle au moment du tir n'est pas bonne du tout, ça peut être la cause de bug mais surtout visuelement c'est degeu
-            StateMachine.transform.LookAt(direction);
+            //StateMachine.transform.LookAt(direction);
             
             StateMachine.Rigidbody.AddForce(direction * force, ForceMode.Impulse);
+            var spinAmount = CalculateSpin(direction, force);
+            StateMachine.Rigidbody.AddTorque(spinAmount, ForceMode.Impulse);
+            
             StateMachine.SwitchState(new PlayerMoveState(StateMachine));
+        }
+        
+        Vector3 CalculateSpin(Vector3 direction, float force)
+        {
+            return new Vector3(direction.z, 0, -direction.x) * force * 0.1f;
         }
 
         #endregion
